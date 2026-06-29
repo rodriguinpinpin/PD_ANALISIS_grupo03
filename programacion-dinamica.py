61ed44c0b837c3b59a936824bb3d82c2d35aa7c3
rodriguinpinpin <amner.cabada@gmail.com>
2026-06-29 16:34:05 -0500

    Agregamos el for para que se pueda ingresar el peso y el valor del objeto

--- programacion-dinamica.py
+++ programacion-dinamica.py
@@ -5,3 +5,7 @@ def mochila_programacion_dinamica():
     
     pesos = [0] * n
     valores = [0] * n
+# Ingrese pesos y valores
+    for i in range(n):
+        pesos[i] = int(input(f"Ingrese el peso del objeto {i + 1}: "))
+        valores[i] = int(input(f"Ingrese el valor del objeto {i + 1}: "))
